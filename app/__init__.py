e30000db45a8580501b8fcdcc01fbf08fc98a63e
marlonlewis2020 <marlonlewis483@gmail.com>
2023-02-12 10:48:25 -0500

    Implemented Environmental configurations

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,18 @@
 from flask import Flask
-# from config import Config
+# from config import ENV 
+# import os
 
 app = Flask(__name__)
 
-# app.config.from_object(Config)
+# env = "production" # env values = [ "default", "production", "development", "testing" ] 
+# env = env.lower()
+
+# if env not in ENV.keys(): env = "default"
+
+# if env == "production": os.environ['FLASK_DEBUG'] = "False"
+    
+# env_object = ENV[env]
+# app.config.from_object(env_object)
+# print("{0} ENVIRONMENT: {1}".format(" *", env_object.__repr__()))
+
 from app import views
